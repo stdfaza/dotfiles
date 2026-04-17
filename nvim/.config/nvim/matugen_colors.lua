460c952fe12efcc8c39e7d16590ea4f5b1e581b8
stdfaza <myemail>
2026-04-17 14:59:26 +0800

    Memperbaiki Struktur folder GNU

---
+++ nvim/.config/nvim/matugen_colors.lua
@@ -0,0 +1,35 @@
+return {
+  -- Core Backgrounds (Deepest Darks)
+  crust     = "#140c0c",
+  mantle    = "#231919",
+  base      = "#1a1111",
+  
+  -- UI Elements & Overlays
+  surface0  = "#271d1d",
+  surface1  = "#322827",
+  surface2  = "#3d3231",
+  overlay0  = "#534342",
+  overlay1  = "#a08c8b",
+  overlay2  = "#d8c1c0",
+  
+  -- Text & Foreground (Brightest Light)
+  subtext0  = "#d8c1c0",
+  subtext1  = "#f1dedd",
+  text      = "#f1dedd",
+  
+  -- Syntax Highlights (Bright Accents ONLY)
+  rosewater = "#ffb3af",
+  flamingo  = "#e2c28c",
+  pink      = "#e2c28c",
+  mauve     = "#e2c28c",
+  red       = "#ffb4ab",
+  maroon    = "#ffb4ab",
+  peach     = "#e7bdba",
+  yellow    = "#e7bdba",
+  green     = "#ffb3af",
+  teal      = "#e7bdba",
+  sky       = "#ffb3af",
+  sapphire  = "#e7bdba",
+  blue      = "#ffb3af",
+  lavender  = "#e2c28c",
+}
